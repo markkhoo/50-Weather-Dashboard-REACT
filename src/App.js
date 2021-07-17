8e3af1044dcd14caa24e1b3b27b11e7cbb98a8e8
markeezy <markkhoo95@gmail.com>
2021-07-16 17:56:57 -0700

    basic form functionality added (needs to be in own component with useContext)

--- src/App.js
+++ src/App.js
@@ -4,11 +4,34 @@ import Header from "./components/header/header";
 import './App.css';
 
 function App() {
+  const [getCity, setCity] = useState([]);
 
+  const writeCity = event => {
+    setCity(event.target.value.toLowerCase());
+
+    // console.log(getCity);
+  };
+
+  const submitCity = event => {
+    event.preventDefault();
+
+    // console.log("city submitted");
+  };
 
   return (
     <div>
       <Header />
+
+      <form
+        onSubmit={submitCity}
+      >
+        <input
+          type="text"
+          onChange={writeCity}
+
+          placeholder="San Francisco"
+        />
+      </form>
 
     </div>
   );
